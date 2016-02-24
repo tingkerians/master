75280ef7e7442fac9df209a7a4412790ea530cb6
khemer d great <khemerdgreat@tingker-mac-server-8gb.local>
2016-02-24 10:20:14 +0800

    adding configuration and module group

---
+++ doharmony/config.swift
@@ -0,0 +1,11 @@
+//
+//  config.swift
+//  doharmony
+//
+//  Created by khemer d great on 23/02/2016.
+//  Copyright © 2016 Eleazer Toluan. All rights reserved.
+//
+
+enum env: String{
+    case apiURL = "192.168.0.1/api/"
+}
